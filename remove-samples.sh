8887368c3672bf701bc8e0a9bd1e8533903b0eb1
qiao_he <qiao_he@hunliji.com>
2023-03-24 16:29:37 +0800

    修复清空范例shell脚本

--- remove-samples.sh
+++ remove-samples.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-rm -f ./*-adapter/src/main/java/com/abc/*/adapter/external/**/*.java
-rm -f ./*-adapter/src/main/java/com/abc/*/adapter/external/clients/rest/ServiceMockRest.java
-rm -f ./*-adapter/src/main/java/com/abc/*/adapter/domain/**/*.java
-rm -f ./*-adapter/src/main/java/com/abc/*/adapter/portal/api/controller/app/AccountController.java
-rm -f ./*-adapter/src/main/java/com/abc/*/adapter/portal/api/controller/app/BillController.java
-rm -f ./*-adapter/src/main/java/com/abc/*/adapter/portal/api/controller/app/OrderController.java
-rm -f ./*-adapter/src/main/java/com/abc/*/adapter/portal/jobs/*.java
-rm -f ./*-adapter/src/main/java/com/abc/*/adapter/portal/queues/*.java
+rm -f ./*-adapter/src/main/java/com/*/*/adapter/external/*/*.java
+rm -f ./*-adapter/src/main/java/com/*/*/adapter/external/*/*/*.java
+rm -f ./*-adapter/src/main/java/com/*/*/adapter/domain/repositories/*.java
+rm -f ./*-adapter/src/main/java/com/*/*/adapter/portal/api/controller/AccountController.java
+rm -f ./*-adapter/src/main/java/com/*/*/adapter/portal/api/controller/BillController.java
+rm -f ./*-adapter/src/main/java/com/*/*/adapter/portal/api/controller/OrderController.java
+#rm -f ./*-adapter/src/main/java/com/*/*/adapter/portal/jobs/*.java
+#rm -f ./*-adapter/src/main/java/com/*/*/adapter/portal/queues/*.java
 
-rm -f ./*-application/src/main/java/com/abc/*/convention/schemas/*.java
-rm -f ./*-application/src/main/java/com/abc/*/application/**/*.java
-rm -f ./*-application/src/main/java/com/abc/*/application/**/**/*.java
-rm -rf ./*-application/src/main/java/com/abc/*/application/commands/account
-rm -rf ./*-application/src/main/java/com/abc/*/application/commands/bill
-rm -rf ./*-application/src/main/java/com/abc/*/application/commands/order
+rm -f ./*-application/src/main/java/com/*/*/convention/schemas/*.java
+rm -f ./*-application/src/main/java/com/*/*/application/**/*.java
+rm -f ./*-application/src/main/java/com/*/*/application/subscribers/**/*.java
+rm -rf ./*-application/src/main/java/com/*/*/application/commands/account
+rm -rf ./*-application/src/main/java/com/*/*/application/commands/bill
+rm -rf ./*-application/src/main/java/com/*/*/application/commands/order
 
-rm -f ./*-domain/src/main/java/com/abc/*/domain/**/*.java
-rm -f ./*-domain/src/main/java/com/abc/*/domain/**/**/*.java
-rm -rf ./*-domain/src/main/java/com/abc/*/domain/aggregates/relationsamples
-rm -rf ./*-domain/src/main/java/com/abc/*/domain/aggregates/samples
+rm -rf ./*-domain/src/main/java/com/*/*/domain/aggregates/relationsamples
+rm -rf ./*-domain/src/main/java/com/*/*/domain/aggregates/samples
+rm -f ./*-domain/src/main/java/com/*/*/domain/events/**/*.java
+rm -f ./*-domain/src/main/java/com/*/*/domain/services/*.java
 
-rm -f ./*-external/src/main/java/com/abc/*/external/**/*.java
+rm -f ./*-external/src/main/java/com/*/*/external/**/*.java
 
 rm -f ./ddl.sql
